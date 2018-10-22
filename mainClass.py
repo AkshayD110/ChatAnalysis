3b0219e187a419786a9fd89fb527aa7366eaf754
akshayd110 <akshayd110@gmail.com>
2018-10-22 23:57:12 +0530

    sectioning code in to functions

--- mainClass.py
+++ mainClass.py
@@ -2,6 +2,20 @@ wordLoveCount = 0
 herMessage = 0
 hisMessage = 0
 mediaShared = 0
+
+def word_count(word):
+    word_counting_element=0
+    with open(r'', mode='r', encoding='utf8'):
+        for line in f:
+            line=line.lower()
+            if (word in line):
+                word_counting_element=word_counting_element+1
+
+    return word_counting_element
+
+
+'''            
+
 with open(r'C:\Users\Adeshpande\Downloads\documents\temp\WhatsAppChatwithNiru.txt', mode='r', encoding="utf8") as f:
     for line in f:
         line=line.lower()
@@ -24,7 +38,7 @@ with open(r'C:\Users\Adeshpande\Downloads\documents\temp\WhatsAppChatwithNiru.tx
         # looks for the media files passed
         if ('<media omitted>' in line):
             mediaShared = mediaShared+1
-
+'''
 
 print(f"The word love has been used {wordLoveCount} number of times")
 print(f"Akshay has messages {hisMessage} number of times")
