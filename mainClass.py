d6cdec86b985cefc6bbc7e4fb1052cde05e5a768
akshayd110 <akshayd110@gmail.com>
2018-10-23 01:18:52 +0530

    partial commit to get the list of strings from the coutner

--- mainClass.py
+++ mainClass.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 word_love_count = 0
 her_message = 0
 his_message = 0
@@ -12,6 +14,31 @@ def word_count(word):
                 word_counting_element=word_counting_element+1
 
     return word_counting_element
+
+def unique_words():
+    all_words = Counter()
+    only_string = Counter()
+    words_to_ignore = ['akshay', 'niru:', 'deshpande:', '<media', 'omitted>', '-']
+    with open(r'C:\Users\Adeshpande\Downloads\documents\temp\WhatsAppChatwithNiru.txt', mode='r', encoding='utf8') as f:
+        for line in f:
+            line=line.lower()
+            all_words.update(line.split()) #this update works only when line is a list. https://stackoverflow.com/questions/40138124/elegant-way-of-adding-a-set-to-a-counter-in-python
+
+    #below loop removes the default text added to chat from whatsapp
+    for word in list(words_to_ignore):
+        if word in all_words:
+            del all_words[word]
+
+    #to get only the counter containing strings
+    for elements in list(all_words):
+        if elements.isalpha():
+            pass
+
+    #print(f'Top 10 words are {all_words.most_common(1000)}')
+    print(f'There were a total of {len(only_string)} unique words/symbols used in the chat')
+    print(f'These are only strings {only_string}')
+
+unique_words()
 
 word_love_count = word_count('love'.lower())
 her_message = word_count('niru:'.lower())
