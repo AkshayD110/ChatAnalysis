a8ecc04ed4d3c3e652dee05950fd41ae25076e06
akshayd110 <akshayd110@gmail.com>
2018-10-23 00:05:14 +0530

    modularised

--- mainClass.py
+++ mainClass.py
@@ -1,11 +1,11 @@
-wordLoveCount = 0
-herMessage = 0
-hisMessage = 0
-mediaShared = 0
+word_love_count = 0
+her_message = 0
+his_message = 0
+media_shared = 0
 
 def word_count(word):
     word_counting_element=0
-    with open(r'', mode='r', encoding='utf8'):
+    with open(r'C:\Users\Adeshpande\Downloads\documents\temp\WhatsAppChatwithNiru.txt', mode='r', encoding='utf8') as f:
         for line in f:
             line=line.lower()
             if (word in line):
@@ -13,37 +13,15 @@ def word_count(word):
 
     return word_counting_element
 
+word_love_count = word_count('love'.lower())
+her_message = word_count('niru:'.lower())
+his_message = word_count('Akshay Deshpande:'.lower())
+media_shared = word_count('Media omitted'.lower())
 
-'''            
-
-with open(r'C:\Users\Adeshpande\Downloads\documents\temp\WhatsAppChatwithNiru.txt', mode='r', encoding="utf8") as f:
-    for line in f:
-        line=line.lower()
-        wordList=line.split()
-        # look for the word love
-        if ('love' in line):
-            wordLoveCount=wordLoveCount+1
-
-        # looks for the name of the person who has texted
-        try:
-            if (wordList[3] == 'akshay'):
-                hisMessage = hisMessage + 1
-
-            if (wordList[3] == 'niru:'):
-                herMessage = herMessage + 1
-
-        except IndexError:
-            pass
-
-        # looks for the media files passed
-        if ('<media omitted>' in line):
-            mediaShared = mediaShared+1
-'''
-
-print(f"The word love has been used {wordLoveCount} number of times")
-print(f"Akshay has messages {hisMessage} number of times")
-print(f"Niru has messages {herMessage} number of times")
-print(f"Images/Audio files have been share {mediaShared} number of times")
+print(f"The word love has been used {word_love_count} number of times")
+print(f"Akshay has messaged {his_message} number of times")
+print(f"Niru has messaged {her_message} number of times")
+print(f"Images/Audio files have been share {media_shared} number of times")
 
 
 
